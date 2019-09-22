58d78fafac46ab2003cda57cd24ac2da1ca3e6b3
spacejumpunreal <spacejumpunreal@163.com>
2019-09-22 22:09:45 +0800

    stash

--- Engine/Sources/Runtime/Core/FrameLogicModule.cpp
+++ Engine/Sources/Runtime/Core/FrameLogicModule.cpp
@@ -1,7 +1,19 @@
 #include "Runtime/Public/Core/FrameLogicModule.h"
+#include "Runtime/Public/Concurrency/Fiber.h"
+#include <limits>
+
 
 namespace Yes
 {
+	void FrameEvent::Wait()
+	{
+		JobWaitingList::Append(Fiber::GetCurrentFiber());
+	}
+	void FrameEvent::Signal()
+	{
+		JobWaitingList::Pop(std::numeric_limits<size_t>::max());
+	}
+
 	struct FrameLogicModuleImp : FrameLogicModule
 	{
 
--- Engine/Sources/Runtime/Public/Core/FrameLogicModule.h
+++ Engine/Sources/Runtime/Public/Core/FrameLogicModule.h
@@ -1,19 +1,42 @@
 #pragma once
 #include "Runtime/Public/Yes.h"
 #include "Runtime/Public/Core/IModule.h"
-#include "Runtime/Public/Core/ITickable.h"
+#include "Runtime/Public/Misc/Name.h"
+#include "Runtime/Public/Concurrency/JobUtils.h"
 
 namespace Yes
 {
+	class IDatum
+	{
+	public:
+		virtual void Clear() = 0;
+	};
+
+	class FrameEvent : private JobWaitingList
+	{
+	public:
+		void Wait();
+		void Signal();
+	};
+
 	class FrameContext
 	{
 	public:
+		//events
+		FrameEvent* GetFrameEvent(const Name& name);
+		//previous frame
+		FrameContext* GetPreviousFrame();
 	};
+
+	using FrameTaskFunction = IDatum* (*)(IDatum* [], size_t n);
 
 	class FrameLogicModule : public IModule
 	{
 	public:
 		virtual void StartFrame() = 0;
-		virtual void RegisterDatum() = 0;
+		virtual void RegisterDatum(const Name& name) = 0;
+		virtual void RegisterFrameEvent(const Name& name) = 0;
+		virtual void RegisterTask(const Name& outName, FrameTaskFunction func, const Name dependencies[], size_t dependencyCount) = 0;
+		virtual void SetRootTasks(const Name& names, size_t count) = 0;
 	};
 }
